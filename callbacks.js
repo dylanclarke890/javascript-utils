fd249f23add9e6db2c19936106bbe58f1c3e3ec5
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-29 22:15:53 +0100

    added jsdocs to callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,5 +1,15 @@
+/**
+ * Delays execution of a callback and cancels a previously registered callback if it wasn't executed yet.
+ */
 export const delay = (function () {
   let timer = 0;
+  /**
+   * Inner function.
+   *
+   * @param {Function} callback The callback to execute.
+   * @param {number} ms Milliseconds to wait before executing the callback.
+   * @return {undefined}
+   */
   return function (callback, ms) {
     clearTimeout(timer);
     timer = setTimeout(function () {
@@ -8,21 +18,37 @@ export const delay = (function () {
   };
 })();
 
-export function debounce(fn, wait) {
+/**
+ * Debounces a function.
+ *
+ * @param {Function} func A function.
+ * @param {number} wait Wait interval in milliseconds.
+ * @return {Function} A new function, debounced.
+ */
+export function debounce(func, wait) {
   let timer = void 0;
   return function (...args) {
     timer !== undefined && clearTimeout(timer);
     timer = setTimeout(function () {
-      fn(...args);
+      func(...args);
     }, wait);
   };
 }
 
-export function throttle(fn, wait) {
+/**
+ * Throttles a function.
+ *
+ * @see https://www.sitepoint.com/throttle-scroll-events/
+ *
+ * @param {Function} func A function.
+ * @param {number} wait Wait interval in milliseconds.
+ * @return {Function} A new function, throttled.
+ */
+export function throttle(func, wait) {
   let time = Date.now();
   return function (...args) {
     if (time + wait - Date.now() < 0) {
-      fn(...args);
+      func(...args);
       time = Date.now();
     }
   };
