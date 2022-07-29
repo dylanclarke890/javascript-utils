5ccdd6fdb61aeab85b755b76252c9ffb309f9090
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-29 18:18:15 +0100

    added http url regex

--- regexes.js
+++ regexes.js
@@ -1,11 +1,11 @@
 /** A collection of various regexes.*/
 
-const AWS = {
-  //from http://blogs.aws.amazon.com/security/blog/tag/key+rotation
-  access_key: "(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])",
-  host_ip: "\\bip-\\d+-\\d+-\\d+-\\d+\\b",
-  secret_key: "(?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=])",
-};
+// TODO: update to JS RegExp
+//from http://blogs.aws.amazon.com/security/blog/tag/key+rotation
+const AWS_access_key = "(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])";
+const AWS_host_ip = "\\bip-\\d+-\\d+-\\d+-\\d+\\b";
+const AWS_secret_key =
+  "(?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=])";
 const ip_prefix = "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}";
 // allows 0 or 255 as the final octet due to CIDR
 const ip =
@@ -31,3 +31,5 @@ const threshold_simple = "^(-?\\d+(?:\\.\\d+)?)$";
 const label = "\\s*[\\%\\(\\)\\/\\*\\w-][\\%\\(\\)\\/\\*\\w\\s-]*";
 const version = "\\d(\\.\\d+)*";
 const version_lax = version + "-?.*";
+const http_url =
+  /^(?:(?:https?:)\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
