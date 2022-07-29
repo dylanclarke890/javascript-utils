0ea09d4c850ede455e8568834975e8d3e11eb486
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-29 22:31:36 +0100

    added normalize utils to map-coordinates

--- map-coordinates.js
+++ map-coordinates.js
@@ -26,3 +26,28 @@ export const wrapLongitude = (longitude) => {
   }
   return longitude;
 };
+
+/**
+ * Normalizes a latitude so that it is within the range [0, 180].
+ * @param {number|string} latitude Either a number or a string
+ * which can be cast to a number.
+ * @return {number} the normalized latitude.
+ */
+export function normalizeLatitude(latitude) {
+  latitude = clampLatitude(latitude);
+  if (latitude >= 0) {
+    return 90 - latitude;
+  }
+  return 90 + Math.abs(latitude);
+}
+
+/**
+ * Normalizes a longitude so that it is within the range [0, 360].
+ * @param {number} longitude Either a number or a string
+ * which can be cast to a number.
+ * @return {number} the normalized longitude.
+ */
+export function normalizeLongitude(longitude) {
+  longitude = wrapLongitude(longitude);
+  return longitude + 180;
+}
