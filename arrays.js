266dd9784e0a39fb6e66966c81b9f043ef4cd324
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-29 18:27:59 +0100

    added more array utils

--- arrays.js
+++ arrays.js
@@ -6,3 +6,33 @@ export function distinct(arr) {
 export function sumOf(arr) {
   return arr.reduce((a, b) => a + b, 0);
 }
+
+export function clone(arr) {
+  return arr.slice(0);
+}
+
+export function isArrayOfNumber(arr) {
+  return !arr.some((v) => isNaN(v));
+}
+
+export function firstItem(arr) {
+  return arr[0];
+}
+
+export function lastItem(arr) {
+  return arr[arr.length - 1];
+}
+
+export function firstNested(arr) {
+  let nested;
+  for (let val in arr) {
+    if (typeof val === "object") nested = val;
+    break;
+  }
+  return nested;
+}
+
+// TODO: for large arrays the array could take a while to reverse.
+export function firstNested(arr) {
+  return firstNested(arr.reverse());
+}
