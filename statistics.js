0d7d1963d84d42b707f064920e5d08d54df77ce3
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-30 00:22:50 +0100

    added statistics utils

---
+++ statistics.js
@@ -0,0 +1,57 @@
+import { sum, integerDivision, isOdd } from "./math";
+import { sortNums } from "./array";
+
+/**
+ * Computes the mean of the given numbers.
+ * @param {...number} numbers The numbers.
+ * @return {number} The mean.
+ */
+export function mean(...numbers) {
+  return sum(...numbers) / numbers.length;
+}
+
+/**
+ * Computes the median of the given numbers.
+ * @param {...number} numbers The numbers.
+ * @return {number} The median value.
+ */
+export function median(...numbers) {
+  const sorted = sortNums(numbers);
+  const middleIndex = integerDivision(sorted.length, 2);
+  return isOdd(sorted.length)
+    ? sorted[middleIndex]
+    : mean(sorted[middleIndex - 1], sorted[middleIndex]);
+}
+
+/**
+ * Computes the min deviation from a value.
+ * @param {number} value A value.
+ * @return {(...numbers: number[]) => number} A function receiving the numbers
+ * to use to compute the minimum deviation from "value".
+ */
+export function minAbsDeviationFromValue(value) {
+  return function (...numbers) {
+    let minAbsDeviation = Infinity;
+    numbers.map((num) => {
+      const deviation = Math.abs(num - value);
+      if (deviation < minAbsDeviation) {
+        minAbsDeviation = deviation;
+      }
+    });
+    return minAbsDeviation;
+  };
+}
+
+/**
+ * Computes the min deviation from a value excluding that value from the numbers received.
+ * @param {number} value A value.
+ * @return {(...numbers: number[]) => number} A function receiving the numbers to use to
+ * compute the minimum deviation from "value" exluding "value".
+ */
+export function minAbsDeviationFromExcludedValue(value) {
+  return function (...numbers) {
+    return minAbsDeviationFromValue(value)(
+      ...numbers.filter((num) => num !== value)
+    );
+  };
+}
