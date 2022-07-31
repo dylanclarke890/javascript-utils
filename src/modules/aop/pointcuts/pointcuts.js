2208e67a171474b3e7db9e271acd159313aae322
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-31 20:35:31 +0100

    added shorthand file for pointcuts

---
+++ src/modules/aop/pointcuts/pointcuts.js
@@ -0,0 +1,45 @@
+import FunctionCallPointcut from "./FunctionCallPointcut";
+import MethodCallPointcut from "../MethodCallPointcut";
+import ObjectConstructionPointcut from "./ObjectConstructionPointcut";
+import PropertyGettingPointcut from "../PropertyGettingPointcut";
+import PropertySettingPointcut from "../PropertySettingPointcut";
+
+/**
+ * Returns a new function call pointcut.
+ * @return {FunctionCallPointcut} A function call pointcut.
+ */
+export function apply() {
+  return new FunctionCallPointcut();
+}
+
+/**
+ * Returns a new method call pointcut.
+ * @return {MethodCallPointcut} A method call pointcut.
+ */
+export function call() {
+  return new MethodCallPointcut();
+}
+
+/**
+ * Returns a new object construction pointcut.
+ * @return {ObjectConstructionPointcut} An object construction pointcut.
+ */
+export function construct() {
+  return new ObjectConstructionPointcut();
+}
+
+/**
+ * Returns a new property getting pointcut.
+ * @return {PropertyGettingPointcut} A property getting pointcut.
+ */
+export function get() {
+  return new PropertyGettingPointcut();
+}
+
+/**
+ * Returns a new property setting pointcut.
+ * @return {PropertySettingPointcut} A property setting pointcut.
+ */
+export function set() {
+  return new PropertySettingPointcut();
+}
