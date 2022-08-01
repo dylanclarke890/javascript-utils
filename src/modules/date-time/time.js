284f33a981ab39bf0e4d45b644ef50d83e556047
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-02 00:19:35 +0100

    added objToMilliseconds

--- src/modules/date-time/time.js
+++ src/modules/date-time/time.js
@@ -235,3 +235,28 @@ export function prettifyMilliseconds(milliseconds, options = {}) {
 
   return options.colonNotation ? result.join("") : result.join(" ");
 }
+
+const converters = {
+  days: (value) => value * 864e5,
+  hours: (value) => value * 36e5,
+  minutes: (value) => value * 6e4,
+  seconds: (value) => value * 1e3,
+  milliseconds: (value) => value,
+  microseconds: (value) => value / 1e3,
+  nanoseconds: (value) => value / 1e6,
+};
+
+/** Convert an object of time properties to milliseconds: {seconds: 2} → 2000 */
+export function objToMilliseconds(timeDescriptor) {
+  let totalMilliseconds = 0;
+  for (const [key, value] of Object.entries(timeDescriptor)) {
+    if (typeof value !== "number")
+      throw new TypeError(
+        `Expected a \`number\` for key \`${key}\`, got \`${value}\` (${typeof value})`
+      );
+    const converter = converters[key];
+    if (!converter) throw new Error(`Unsupported time key: ${key}`);
+    totalMilliseconds += converter(value);
+  }
+  return totalMilliseconds;
+}
