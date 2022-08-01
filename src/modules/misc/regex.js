a95a4a2db0a373a38a00f4babe504a7da0a41720
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-01 22:47:14 +0100

    added email regex and more notes to regex.js

--- src/modules/misc/regex.js
+++ src/modules/misc/regex.js
@@ -1,11 +1,27 @@
-/** A collection of various regexes.*/
+/**********************************************************************************************
+ *                          C O M M O N
+ */
 
-// TODO: update to JS RegExp
+export const email =
+  "[^\\.\\s@:](?:[^\\s@:]*[^\\s@:\\.])?@[^\\.\\s@]+(?:\\.[^\\.\\s@]+)*";
+export function emailRegex(exact) {
+  return exact ? new RegExp(`^${regex}$`) : new RegExp(regex, "g");
+}
+
+// TODO: update below to JS RegExp
+
+/**********************************************************************************************
+ *                            A W S
+ */
 //from http://blogs.aws.amazon.com/security/blog/tag/key+rotation
 export const AWS_access_key = "(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])";
 export const AWS_host_ip = "\\bip-\\d+-\\d+-\\d+-\\d+\\b";
 export const AWS_secret_key =
   "(?<![A-Za-z0-9/+=])[A-Za-z0-9/+=]{40}(?![A-Za-z0-9/+=])";
+
+/**********************************************************************************************
+ *                      I P  A N D  W E B  S E R V I C E S
+ */
 export const ip_prefix =
   "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}";
 // allows 0 or 255 as the final octet due to CIDR
@@ -38,6 +54,10 @@ export const version_lax = version + "-?.*";
 export const http_url =
   /^(?:(?:https?:)\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
+/**********************************************************************************************
+ *                      F I L T E R  M E T H O D S
+ */
+
 export function filterInt(str) {
   let filtered = str.replace(/[^0-9]/g, "");
   filtered = filtered.replace(/^[0]+([1-9])/, "$1");
@@ -64,15 +84,9 @@ export function filterFloat(str) {
   return filtered;
 }
 
-/**
- * Escapes special characters for a JS regex.
- * @see https://stackoverflow.com/questions/3115150/how-to-escape-regular-expression-special-characters-using-javascript#answer-9310752
- * @param {string} text The string to escape.
- * @return {string} The same string with special regex characters escaped.
+/**********************************************************************************************
+ *                      E S C A P E  R E G E X P  S T R I N G S
  */
-export function escapeRegExp(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-}
 
 /**
  * Escapes special characters for a JS regex. More reliable version of `escapeRegExp(text)`.
@@ -86,4 +100,14 @@ export default function escapeStringRegexp(string) {
   // Escape characters with special meaning either inside or outside character sets.
   // Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
   return string.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&").replace(/-/g, "\\x2d");
+}
+
+/**
+ * Escapes special characters for a JS regex.
+ * @see https://stackoverflow.com/questions/3115150/how-to-escape-regular-expression-special-characters-using-javascript#answer-9310752
+ * @param {string} text The string to escape.
+ * @return {string} The same string with special regex characters escaped.
+ */
+export function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
