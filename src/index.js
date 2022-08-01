8e2e36381dbccce63dd44a2332677f9ef98c23d9
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-01 22:15:22 +0100

    moved AOP entry to main entry

--- src/index.js
+++ src/index.js
@@ -241,3 +241,59 @@ export {
   lazyMap,
   lazyMapFactory,
 };
+
+/******************************************************************
+ |                  A O P  P R O X Y
+*/
+
+import AOPProxy, { EFFECTIVE_TARGET_PROP } from "./modules/aop/AOPProxy";
+import applyRule from "./modules/aop/rules/applyRule";
+import constructRule from "./modules/aop/rules/constructRule";
+import {
+  call,
+  get,
+  set,
+  apply,
+  construct,
+} from "./modules/aop/pointcuts/pointcuts";
+import lazyObject from "./modules/aop/builtins/lazyObject";
+import throwErrorForUnknownProperty from "./modules/aop/builtins/throwErrorForUnknownProperty";
+import POJOPromiser from "./modules/aop/builtins/POJOPromiser";
+import arrayWithNegativeIndices from "./modules/object-handling/arrays";
+
+/*
+ * Mapping the API properties to the pigretto function.
+ */
+AOPProxy.pointcuts = {};
+[
+  ["call", call],
+  ["get", get],
+  ["set", set],
+  ["applyRule", applyRule],
+  ["apply", apply],
+  ["constructRule", constructRule],
+  ["construct", construct],
+].map(([prop, val]) => (AOPProxy.pointcuts[prop] = val));
+
+AOPProxy.builtin = {};
+[
+  ["lazyObject", lazyObject],
+  ["throwErrorForUnknownProperty", throwErrorForUnknownProperty],
+  ["POJOPromiser", POJOPromiser],
+  ["arrayWithNegativeIndices", arrayWithNegativeIndices],
+].map(([prop, val]) => (AOPProxy.builtin[prop] = val));
+
+AOPProxy.constants = {};
+[["EFFECTIVE_TARGET_PROP", EFFECTIVE_TARGET_PROP]].map(
+  ([prop, val]) => (AOPProxy.constants[prop] = val)
+);
+
+export default AOPProxy;
+export { call, get, set, applyRule, apply, constructRule, construct };
+export {
+  lazyObject,
+  throwErrorForUnknownProperty,
+  POJOPromiser,
+  arrayWithNegativeIndices,
+};
+export { EFFECTIVE_TARGET_PROP };
