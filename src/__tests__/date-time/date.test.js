bf95be807ddde4035e426ee0154f09cebb2b45e1
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-03 00:10:00 +0100

    added tests for isValidDate

--- src/__tests__/date-time/date.test.js
+++ src/__tests__/date-time/date.test.js
@@ -1,5 +1,17 @@
 import { isValidDate } from "../../modules/date-time/date";
 
 describe("isValidDate", () => {
-  test("", () => {});
+  test("handles undefined argument", () => {
+    expect(isValidDate(undefined)).toBe(false);
+  });
+
+  test("returns false for invalid date", () => {
+    const invalidArgs = [3, "t", [], {}, () => null, null, Symbol("t")];
+    for (let invalid in invalidArgs) expect(isValidDate(invalid)).toBe(false);
+  });
+
+  test("returns true for valid date", () => {
+    const date = new Date();
+    expect(isValidDate(date)).toBe(true);
+  });
 });
