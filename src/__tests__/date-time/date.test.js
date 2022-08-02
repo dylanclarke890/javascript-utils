10f9eff15ad329fb82c0c0fd381112e51f4f4ad3
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-03 00:16:15 +0100

    added test cases for isValidDateTimeStr

--- src/__tests__/date-time/date.test.js
+++ src/__tests__/date-time/date.test.js
@@ -1,4 +1,4 @@
-import { isValidDate } from "../../modules/date-time/date";
+import { isValidDate, isValidDateTimeStr } from "../../modules/date-time/date";
 
 describe("isValidDate", () => {
   test("handles undefined argument", () => {
@@ -13,5 +13,26 @@ describe("isValidDate", () => {
   test("returns true for valid date", () => {
     const date = new Date();
     expect(isValidDate(date)).toBe(true);
+  });
+});
+
+describe("isValidDateTimeStr", () => {
+  test("handles undefined argument", () => {
+    expect(isValidDateTimeStr(undefined)).toBe(false);
+  });
+  test("returns false for invalid string", () => {
+    expect(isValidDateTimeStr("NotADate")).toBe(false);
+  });
+  test("returns true for valid string", () => {
+    const validStrings = [
+      "2018-12-30",
+      "2018-12-30T20:59",
+      "2018-12-30T20:59:00",
+      "2018-12-30T20:59:00.000Z",
+      "2018-12-30T20:59:00.000+01:00",
+      "2018-12-30T20:59:00.000-01:00",
+    ];
+    for (let valid in validStrings)
+      expect(isValidDateTimeStr(valid)).toBe(true);
   });
 });
