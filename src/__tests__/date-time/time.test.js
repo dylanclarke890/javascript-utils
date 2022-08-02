497adb77a78c88d39127afbae0774c2189901e9e
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-03 00:21:41 +0100

    added initial file for time test cases

---
+++ src/__tests__/date-time/time.test.js
@@ -0,0 +1,19 @@
+import {
+  now,
+  time,
+  msToTime,
+  secsToTimeString,
+  parseMilliseconds,
+  prettifyMilliseconds,
+  objToMilliseconds,
+  timeZone,
+} from "../../modules/date-time/time";
+
+// TODO: test cases for now
+// TODO: test cases for time
+// TODO: test cases for msToTime
+// TODO: test cases for secsToTimeString
+// TODO: test cases for parseMilliseconds
+// TODO: test cases for prettifyMilliseconds
+// TODO: test cases for objToMilliseconds
+// TODO: test cases for timeZone
