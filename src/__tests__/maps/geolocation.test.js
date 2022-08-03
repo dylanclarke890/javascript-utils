d125355af3726a9bcff6e9651bf029a0cfd3f82f
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-03 10:36:39 +0100

    added rough outline for tests in geolocation

---
+++ src/__tests__/maps/geolocation.test.js
@@ -0,0 +1,31 @@
+import {
+  isSamePosition,
+  makePositionObj,
+} from "../../modules/maps/geolocation";
+
+// TODO: test cases for isSamePosition
+// TODO: test cases for makePositionObj
+
+describe("isSamePosition", () => {
+  test("handles invalid args", () => {
+    throw new Error("Not Implemented!");
+  });
+  test("returns false for inequal objects", () => {
+    throw new Error("Not Implemented!");
+  });
+  test("returns true for equal objects", () => {
+    throw new Error("Not Implemented!");
+  });
+});
+
+describe("makePositionObj", () => {
+  test("handles invalid args", () => {
+    throw new Error("Not Implemented!");
+  });
+  test("returns expected obj", () => {
+    throw new Error("Not Implemented!");
+  });
+  test("returns true for equal objects", () => {
+    throw new Error("Not Implemented!");
+  });
+});
