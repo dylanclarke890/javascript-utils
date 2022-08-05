49538ed7aaf7db0c564d36d02225d57f36d2273c
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-05 05:20:56 +0100

    added debounce func test

--- src/__tests__/misc/callback.tests.js
+++ src/__tests__/misc/callback.tests.js
@@ -3,9 +3,30 @@ import { throttle, debounce, delay } from "../../modules/misc/callbacks";
 jest.useFakeTimers();
 jest.spyOn(global, "setTimeout");
 
-test("waits 1 second before ending the game", () => {
-  const funcToDelay = () => null;
+test("delay waits one seconds before calling delayed func", () => {
+  const funcToDelay = jest.fn();
   delay(funcToDelay, 1000);
+
+  expect(funcToDelay).not.toBeCalled();
   expect(setTimeout).toHaveBeenCalledTimes(1);
   expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+
+  jest.runAllTimers();
+  expect(funcToDelay).toHaveBeenCalledTimes(1);
+});
+
+test("debounce returns a function with a delay", () => {
+  const funcToDelay = jest.fn();
+
+  const debounced = debounce(funcToDelay, 1000);
+  expect(funcToDelay).not.toBeCalled();
+  expect(setTimeout).toBeCalled();
+
+  debounced();
+  expect(setTimeout).toBeCalled();
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+  expect(funcToDelay).not.toBeCalled();
+
+  jest.runAllTimers();
+  expect(funcToDelay).toHaveBeenCalledTimes(1);
 });
