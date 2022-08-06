5aa38e41543f4ecf9b4dca980456a7bbc27e1576
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-06 16:32:43 +0100

    added test cases for nested properties

---
+++ src/__tests__/object-handling/nested-properties.test.js
@@ -0,0 +1,100 @@
+import {
+  getNestedPropertyValue,
+  hasNestedPropertyValue,
+  nestedMapGet,
+  nestedMapHas,
+  nestedMapSet,
+  nestedTreeMapHas,
+  nestedTreeMapSet,
+  setNestedPropertyValue,
+} from "../../modules/object-handling/nested-properties";
+
+test("getNestedPropertyValue returns expected object", () => {
+  const source = { a: { b: { c: { d: 4 } } } };
+  expect(getNestedPropertyValue(source, ["a", "b", "c", "d"])).toStrictEqual(4);
+});
+
+test("hasNestedPropertyValue returns expected bolean", () => {
+  const source = { a: { b: { c: { d: 4 } } } };
+  expect(hasNestedPropertyValue(source, ["a", "b", "c", "d"])).toBe(true);
+  expect(hasNestedPropertyValue(source, ["e", "f"])).toBe(false);
+});
+
+test("setNestedPropertyValue sets expected value", () => {
+  const source = { a: { b: { c: { d: 4 } } } };
+  setNestedPropertyValue(source, ["a", "b", "c", "d"], 10);
+  expect(source.a.b.c.d).toBe(10);
+});
+
+test("nestedMapSet sets expected value", () => {
+  const mapA = new Map();
+  const mapB = new Map();
+  const mapC = new Map();
+  const mapD = new Map();
+  mapD.set("d", 4);
+  mapC.set("c", mapD);
+  mapB.set("b", mapC);
+  mapA.set("a", mapB);
+  nestedMapSet(mapA, ["a", "b", "c", "d"], 10);
+  expect(mapD.get("d")).toBe(10);
+});
+
+test("nestedMapHas returns expected boolean", () => {
+  const mapA = new Map();
+  const mapB = new Map();
+  const mapC = new Map();
+  const mapD = new Map();
+  mapD.set("d", 4);
+  mapC.set("c", mapD);
+  mapB.set("b", mapC);
+  mapA.set("a", mapB);
+  expect(nestedMapHas(mapA, ["a", "b", "c", "d"])).toBe(true);
+});
+
+test("nestedMapGet returns expected value", () => {
+  const mapA = new Map();
+  const mapB = new Map();
+  const mapC = new Map();
+  const mapD = new Map();
+  mapD.set("d", 4);
+  mapC.set("c", mapD);
+  mapB.set("b", mapC);
+  mapA.set("a", mapB);
+  expect(nestedMapGet(mapA, ["a", "b", "c", "d"])).toBe(4);
+});
+
+test("nestedTreeMapHas returns expected boolean", () => {
+  const map = new Map(
+    new Map([
+      ["whole numbers", [1, 2, 3, 4]],
+      ["Decimal numbers", [1.1, 1.2, 1.3, 1.4]],
+      ["negative numbers", [-1, -2, -3, -4]],
+    ])
+  );
+  expect(nestedTreeMapHas(map, ["whole numbers"])).toBe(true);
+});
+
+test("nestedTreeMapSet", () => {
+  const map = new Map(
+    new Map([
+      ["whole numbers", [0]],
+      ["Decimal numbers", [1.1, 1.2, 1.3, 1.4]],
+      ["negative numbers", [-1, -2, -3, -4]],
+    ])
+  );
+  nestedTreeMapSet(map, ["whole numbers"], [1]);
+  // expect(nestedMapGet(map, ["whole numbers"])).toStrictEqual([1]);
+});
+
+test("nestedTreeMapGet", () => {
+  const map = new Map(
+    new Map([
+      ["whole numbers", [0]],
+      ["Decimal numbers", [1.1, 1.2, 1.3, 1.4]],
+      ["negative numbers", [-1, -2, -3, -4]],
+    ])
+  );
+  expect(nestedMapGet(map, ["whole numbers"])).toStrictEqual([0]);
+});
+
+test.todo("nestedObjectConstructValue");
--- src/modules/object-handling/nested-properties.js
+++ src/modules/object-handling/nested-properties.js
@@ -1,7 +1,10 @@
 /**
  * Gets a nested value of an object given an array of nested property names (keys).
  * @param {Object} data JS POJO object.
- * @param {Array} props Array of object nested keys.
+ * @param {Array} props Array of keys in order of nesting.
+ * @example
+ * const source = { a: { b: { c: { d: 4 } } } };
+ * getNestedPropertyValue(source, ["a", "b", "c", "d"]) === 4; // true
  * @return {*} The leaf value.
  */
 export function getNestedPropertyValue(data, props) {
@@ -16,7 +19,7 @@ export function getNestedPropertyValue(data, props) {
 /**
  * Checks if a nested value of an object given an array of nested property names (keys) exists.
  * @param {Object} data JS POJO object.
- * @param {Array} props Array of object nested keys.
+ * @param {Array} props Array of keys in order of nesting.
  * @return {boolean} True if the nested key exists, false otherwise.
  */
 export function hasNestedPropertyValue(data, props) {
@@ -33,13 +36,12 @@ export function hasNestedPropertyValue(data, props) {
 /**
  * Sets a nested value of an object given an array of nested property names (keys).
  * @param {Object} data JS POJO object.
- * @param {Array} props Array of object nested keys.
+ * @param {Array} props Array of keys in order of nesting.
  * @param {*} value Leaf value.
  * @return {undefined}
  */
 export function setNestedPropertyValue(data, props, value) {
   if (!props.length) return;
-  
   let root = data;
   let prev = null;
   for (let i = 0; i < props.length; i++) {
@@ -53,7 +55,6 @@ export function setNestedPropertyValue(data, props, value) {
 
 /**
  * Sets a nested value on a nested map.
- *
  * @param {Map|WeakMap} map A map or weak map.
  * @param {Array} keys Array of keys to traverse. Each key will lead to a nested map.
  * @param {*} value The value to set at the inner key.
@@ -65,9 +66,8 @@ export function nestedMapSet(map, keys, value) {
   while (i < keys.length - 1) {
     const key = keys[i];
     const nested = current.get(key);
-    if (nested instanceof Map || nested instanceof WeakMap) {
-      current = nested;
-    } else {
+    if (nested instanceof Map || nested instanceof WeakMap) current = nested;
+    else {
       const newMap = new Map();
       current.set(key, newMap);
       current = newMap;
@@ -79,7 +79,6 @@ export function nestedMapSet(map, keys, value) {
 
 /**
  * Tests if a map has the given nested keys.
- *
  * @param {Map|WeakMap} map A map or weak map.
  * @param {Array} keys Array of keys to check. Each key represents a nested map.
  * @return {boolean} "true" if all the nested keys exist, false otherwise.
@@ -127,24 +126,24 @@ const treeMapSubtree = Symbol("treeMapSubtree");
 
 /**
  * Sets a nested value on a nested tree map.
- * @param {Map|WeakMap} rootMap A map or weak map to use as the root.
+ * @param {Map|WeakMap} root A map or weak map to use as the root.
  * @param {Array} keys Array of keys to traverse. Each key will lead to a nested node of the tree map.
  * @param {*} value The value to set at the inner nested key.
  * @return {undefined}
  */
-export function nestedTreeMapSet(rootMap, keys, value) {
+export function nestedTreeMapSet(root, keys, value) {
   let i = 0;
-  let current = rootMap;
-  const MapConstructor = rootMap instanceof WeakMap ? WeakMap : Map;
+  let current = root;
+  const mapInstance = root instanceof WeakMap ? WeakMap : Map;
   while (i < keys.length - 1) {
     const key = keys[i];
     const nested = current.get(key);
-    if (nested) 
+    if (nested)
       current =
         nested[treeMapSubtree] ||
-        (nested[treeMapSubtree] = new MapConstructor());
+        (nested[treeMapSubtree] = new mapInstance());
     else {
-      const newMap = new MapConstructor();
+      const newMap = new mapInstance();
       const node = {
         [treeMapSubtree]: newMap,
         value: void 0,
@@ -213,18 +212,18 @@ export function nestedTreeMapGet(rootMap, keys) {
 
 /**
  * Nests the properties of an object using an array of props definitions and defaults.
- * Returns the leaf.
  * @param {Object} root The root object.
- * @param {Array} nestedPropsDef The nexted props definitions.
- * @param {boolean} [rootArrayIfFalse] True if the root should be an array if the first argument "root" is falsy.
+ * @param {Array} nestedPropsDef The nested props definitions.
+ * @param {boolean} [arrOnFail] True if the root should be an array if the first argument
+ * "root" is falsy.
  * @return {*} The leaf.
  */
 export function nestedObjectConstructValue(
   root,
   nestedPropsDef,
-  rootArrayIfFalse
+  arrOnFail
 ) {
-  root = root || (rootArrayIfFalse ? [] : {});
+  root = root || (arrOnFail ? [] : {});
   let leaf = root;
   for (let i = 0; i < nestedPropsDef.length; i++) {
     const propDef = nestedPropsDef[i];
