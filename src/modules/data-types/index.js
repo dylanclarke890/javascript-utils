0db06b6f317094e56c19223ae043fdb69e62b801
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-07 00:16:47 +0100

    added index file for data-types

---
+++ src/modules/data-types/index.js
@@ -0,0 +1,5 @@
+export * from "./DeferredPromise";
+export * from "./LinkedQueue";
+export * from "./StringBuilder";
+export * from "./ILOM/ImmutableLinkedOrderedMap";
+export * from "./ILOM/shortcuts";
