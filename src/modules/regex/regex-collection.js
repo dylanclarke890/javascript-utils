5a6da65370215f4822d33549d3f27254dab9e94a
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-07 14:28:54 +0100

    removed duplicate

--- src/modules/regex/regex-collection.js
+++ src/modules/regex/regex-collection.js
@@ -11,8 +11,6 @@ export const domain = "\\b[a-zA-Z0-9](?:[a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\b";
 export const rwxt = "[r-][w-][x-][r-][w-][x-][r-][w-][xt-]";
 export const subnet =
   "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[1-9][0-9]|[01]?0[1-9]|[12]00|[0-9])\\b";
-export const mac =
-  "\\b[0-9A-Fa-f]{1,2}[:-](?:[0-9A-Fa-f]{1,2}[:-]){4}[0-9A-Fa-f]{1,2}\\b";
 export const process_name = "\\s*[\\w./<>-][\\w\\s./<>-]+";
 export const url_path_suffix = "/(?:[\\w.,:/%&?#!=*|\\[\\]~+-]+)?"; // there is an RFC3987 regex but it's gigantic, this is easier to reason about and serves most needs
 export const user = "\\b[A-Za-z0-9][A-Za-z0-9\\._-]*[A-Za-z0-9]\\b";
