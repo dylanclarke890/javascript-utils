82c486819c6e1ade3903af4e8efca12c38ab284d
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-07 22:12:46 +0100

    moved aop proxy export to its own file

---
+++ src/modules/aop/index.js
@@ -0,0 +1,57 @@
+import AOPProxy, { EFFECTIVE_TARGET_PROP } from "./modules/aop/AOPProxy";
+import applyRule from "./modules/aop/rules/applyRule";
+import constructRule from "./modules/aop/rules/constructRule";
+import {
+  call,
+  get,
+  set,
+  apply,
+  construct,
+} from "./modules/aop/pointcuts/pointcuts";
+import lazyObject from "./modules/aop/builtins/lazyObject";
+import throwErrorForUnknownProperty from "./modules/aop/builtins/throwErrorForUnknownProperty";
+import POJOPromiser from "./modules/aop/builtins/POJOPromiser";
+import arrayWithNegativeIndices from "./modules/object-handling/arrays";
+
+/*
+ * Mapping the API properties to the proxy function.
+ */
+AOPProxy.pointcuts = {};
+[
+  ["call", call],
+  ["get", get],
+  ["set", set],
+  ["applyRule", applyRule],
+  ["apply", apply],
+  ["constructRule", constructRule],
+  ["construct", construct],
+].map(([prop, val]) => (AOPProxy.pointcuts[prop] = val));
+
+AOPProxy.builtin = {};
+[
+  ["lazyObject", lazyObject],
+  ["throwErrorForUnknownProperty", throwErrorForUnknownProperty],
+  ["POJOPromiser", POJOPromiser],
+  ["arrayWithNegativeIndices", arrayWithNegativeIndices],
+].map(([prop, val]) => (AOPProxy.builtin[prop] = val));
+
+AOPProxy.constants = {};
+[["EFFECTIVE_TARGET_PROP", EFFECTIVE_TARGET_PROP]].map(
+  ([prop, val]) => (AOPProxy.constants[prop] = val)
+);
+
+export {
+  AOPProxy,
+  call,
+  get,
+  set,
+  applyRule,
+  apply,
+  constructRule,
+  construct,
+  lazyObject,
+  throwErrorForUnknownProperty,
+  POJOPromiser,
+  arrayWithNegativeIndices,
+  EFFECTIVE_TARGET_PROP,
+};
