cdb60f3c35a80fffe443af1c96e01b34392078ed
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-07 14:50:17 +0100

    removed redundant regexes

--- src/modules/regex/regex-collection.js
+++ src/modules/regex/regex-collection.js
@@ -1,15 +1,8 @@
-export const ip_prefix =
-  "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}";
-// allows 0 or 255 as the final octet due to CIDR
-export const ip =
-  ip_prefix +
-  "(?:25[0-5]|2[0-4][0-9]|[01]?[1-9][0-9]|[01]?0[1-9]|[12]00|[0-9])\\b";
 // must permit numbers as valid host identifiers that are being used in the wild in FQDNs
 export const hostname =
   "\\b[A-Za-z0-9](?:[A-Za-z0-9_\\-]{0,61}[a-zA-Z0-9])?\\b";
 export const rwxt = "[r-][w-][x-][r-][w-][x-][r-][w-][xt-]";
 export const process_name = "\\s*[\\w./<>-][\\w\\s./<>-]+";
-export const url_path_suffix = "/(?:[\\w.,:/%&?#!=*|\\[\\]~+-]+)?"; // there is an RFC3987 regex but it's gigantic, this is easier to reason about and serves most needs
 export const user = "\\b[A-Za-z0-9][A-Za-z0-9\\._-]*[A-Za-z0-9]\\b";
 export const column = "\\b[\\w:]+\\b";
 export const threshold_range =
